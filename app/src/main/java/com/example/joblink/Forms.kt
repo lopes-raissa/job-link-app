5e016198fcbce2d96103703cce1c0cbd00d8d3b1
RAISSA LOPES SANTANA <20185494@CFP127ALUNO.LOCAL>
2021-04-26 16:56:59 -0300

    Criação da tela do Splash e início da tela de formulário.

---
+++ app/src/main/java/com/example/joblink/Forms.kt
@@ -0,0 +1,11 @@
+package com.example.joblink
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Forms : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forms)
+    }
+}
