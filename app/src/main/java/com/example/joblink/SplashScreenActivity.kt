df6af8a62240efca306940ab2da269390c7c2c6d
MATHEUS HENRIQUE SANTOS DA SILVA <20185514@CFP127ALUNO.LOCAL>
2021-05-09 16:51:44 -0300

    Ajustando views

--- app/src/main/java/com/example/joblink/SplashScreenActivity.kt
+++ app/src/main/java/com/example/joblink/SplashScreenActivity.kt
@@ -3,6 +3,7 @@ package com.example.joblink
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
@@ -10,20 +11,18 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-
-
-        window_splash.setOnClickListener {
-
-            val abrirActivityMain = Intent(this, MainActivity::class.java)
-            startActivity(abrirActivityMain)
-
+        changeToLogin()
         }
+    fun changeToLogin(){
+        val intent = Intent(this, MainActivity::class.java)
 
+        Handler().postDelayed({
+            intent.change()
+        }, 2000)
+    }
 
-//        Handler().postDelayed({
-//            startActivity(Intent(this, SplashScreenActivity::class.java))
-//            finish()
-//
-//        }, 3000)
+    fun Intent.change(){
+        startActivity(this)
+        finish()
     }
 }
