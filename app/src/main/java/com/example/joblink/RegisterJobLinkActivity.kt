ad8ed76fac94e11dbd96b5141edf6777663580cb
Matheus Henrique <matheustennant@gmail.com>
2021-05-14 15:39:34 -0300

    adicionando dependencias

--- app/src/main/java/com/example/joblink/RegisterJobLinkActivity.kt
+++ app/src/main/java/com/example/joblink/RegisterJobLinkActivity.kt
@@ -6,9 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import com.example.joblink.http.HttpHelper
 import com.example.joblink.model.JobLink
-import com.google.gson.Gson
 import org.jetbrains.anko.doAsync
 
 class RegisterJobLinkActivity : AppCompatActivity() {
@@ -21,35 +19,6 @@ class RegisterJobLinkActivity : AppCompatActivity() {
 
         buttonVoltar.setOnClickListener {
             onBackPressed()
-        }
-
-        val buttonSalvar = findViewById<Button>(R.id.button_criar_conta)
-        val editTextNome = findViewById<EditText>(R.id.edit_text_name)
-        val editTextSexo = findViewById<EditText>(R.id.edit_text_sexo)
-        val editTextNascimento = findViewById<EditText>(R.id.edit_text_nascimento)
-        val editTextEmail = findViewById<EditText>(R.id.edit_text_email_register)
-        val editTextCpf = findViewById<EditText>(R.id.edit_text_cpf)
-        val editTextSenha = findViewById<EditText>(R.id.edit_text_senha_register)
-
-        buttonSalvar.setOnClickListener {
-            // criando um objeto Joblink
-            val cliente = JobLink()
-            cliente.name = editTextNome.text.toString()
-            cliente.sexo = editTextSexo.text.toString()
-            cliente.birthDate = editTextNascimento.text.toString()
-            cliente.email = editTextEmail.text.toString()
-            cliente.cpf = editTextCpf.text.toString()
-            cliente.password = editTextSenha.text.toString()
-
-            //converte em json
-            val gson = Gson()
-            val clienteJson = gson.toJson(cliente)
-
-            doAsync {
-                val http = HttpHelper()
-                http.post(clienteJson)
-            }
-
         }
     }
 }
